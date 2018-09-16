69ead43a1563c2541fcb220bc05a1f8bcf66a506
HM <z.ylime@hotmail.com>
2018-09-16 11:54:44 -0300

    init project

---
+++ public/assets/js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+$(document).ready(function () {
+  banner();
+
+  function banner() {
+    $('.js-banner').slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      arrows: false
+    });
+  }
+});
